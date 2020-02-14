c4cff5d47dd4778767644d0f9f38a8694b8e070e
xp <xp@CETYSDAM00>
2020-02-14 17:32:33 +0100

    primer algoritmo burbuja

--- src/algoritmosordenacion/AlgoritmosOrdenacion.java
+++ src/algoritmosordenacion/AlgoritmosOrdenacion.java
@@ -18,12 +18,12 @@ public class AlgoritmosOrdenacion {
          * metodo que recibe un array de numeros y lo ordenará a través del
          * algoritmo de la Burbuja
          */
-        for (int j = 0; j < numeros.length; j++) { //se hace en un bucle for para que de todas vueltas necesarias
-            for (int i = 0; i < numeros.length; i++) {
+         
+        for (int j = 0; j < numeros.length; j++) {
+            for (int i = 0; i < numeros.length - 1; i++) {
                 if (numeros[i] > numeros[i + 1]) {
-                    //si se cumple. intercambio los valores de i e i+1
-
-                    int aux = numeros[i + 1];// se añade un aux para no pisar valores
+                    //si se cumple, intercambio los valores de i e i+1
+                    int aux = numeros[i + 1];
                     numeros[i + 1] = numeros[i];
                     numeros[i] = aux;
                 }
@@ -35,12 +35,16 @@ int[]lista1 = {13,47,44,36,32,61,31,555,666,1988,1987,46};
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        AlgoritmosOrdenacion ordenacion= new AlgoritmosOrdenacion();
-           //los arrays se imprimen de forma diferente
-        System.out.println("Lista sin ordenar:" +Arrays.toString(ordenacion.lista1));
+        //  //los arrays se imprimen de forma diferente
+       AlgoritmosOrdenacion ordenacion =new AlgoritmosOrdenacion();
+        System.out.println("lista sin ordenar:" 
+                + Arrays.toString(ordenacion.lista1));
+        
         ordenacion.ordenacionBurbuja(ordenacion.lista1);
         System.out.println("Lista ordenada:" +Arrays.toString(ordenacion.lista1));
     }
+       
+        
 
 }
+
